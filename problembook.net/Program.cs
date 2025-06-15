5f11bfa420307de5d8881486307093c45e2388fd
Oleg <aleynikov.oleg@gmail.com>
2025-06-15 13:27:02 +0300

    problembook partial

--- problembook.net/Program.cs
+++ problembook.net/Program.cs
@@ -1,111 +1,104 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
-
-
-
-    class Program
+    partial class Program
     {
+        #region перегрузки см. вызывающий код
+        public static void Foo(object a) => Console.WriteLine("object"); // TODO public везде из-за partial
 
-        static void Foo(object a)
-        {
-            Console.WriteLine("object");
-        }
-        static void Foo(object a, object b)
-        {
-            Console.WriteLine("object, object");
-        }
-        static void Foo(params object[] args)
-        {
-            Console.WriteLine("params object[]");
-        }
-        static void Foo<T>(params T[] args)
-        {
-            Console.WriteLine("params T[]");
-        }
-        class Bar { }
+        public static void Foo(object a, object b) => Console.WriteLine("object, object");
 
+        public static void Foo(params object[] args) => Console.WriteLine("params object[]");
 
+        public static void Foo<T>(params T[] args) => Console.WriteLine("params T[]");
+        
+        public class Bar { }
+        #endregion
 
-        class Foo2
+        public class Foo2
         {
             public virtual void Quux(int a)
             {
                 Console.WriteLine("Foo.Quux(int)");
             }
         }
-        class Bar2 : Foo2
+
+        public class Bar2 : Foo2
         {
             public override void Quux(int a)
             {
                 Console.WriteLine("Bar.Quux(int)");
             }
+
             public void Quux(object a)
             {
                 Console.WriteLine("Bar.Quux(object)");
             }
         }
-        class Baz : Bar2
+
+        public class Baz : Bar2
         {
             public override void Quux(int a)
             {
                 Console.WriteLine("Baz.Quux(int)");
             }
+
             public void Quux<T>(params T[] a)
             {
                 Console.WriteLine("Baz.Quux(params T[])");
             }
         }
 
-        class Foo3
+
+
+        public class Foo3
         {
             public Foo3()
             {
                 Quux();
             }
+
             public virtual void Quux()
             {
                 Console.WriteLine("Foo.Quux()");
             }
         }
-        class Bar3 : Foo3
+
+        public class Bar3 : Foo3
         {
             protected string name;
+
             public Bar3()
             {
                 name = "Bar";
             }
+
             public override void Quux()
             {
                 Console.WriteLine("Bar.Quux(), " + name);
             }
-            public void Quux(params object[] args)
+
+            public void Quux(params object[] args) // 2 Quux. одинаковую сигнатуру нельзя
             {
                 Console.WriteLine("Bar.Quux(params object[])");
             }
         }
-        class Baz3 : Bar3
+
+        public class Baz3 : Bar3
         {
+            int a1;
             public Baz3()
             {
                 name = "Baz";
-                Quux();
-                ((Foo3)this).Quux();
+                Quux(); // TODO п1 вызовется Quux(params object[] args), если бы его не было - Quux(). почему?
+                ((Foo3)this).Quux(); // this - текущий экземпляр Baz3. в дебаге показывает a1 и унаследованный name (как будто его)
             }
         }
 
 
 
-        class Foo4
+        public class Base4
         {
+            //class Quux // предупреждение, ошибка. internal - c# default class access modifier
             protected class Quux
             {
                 public Quux()
@@ -114,8 +107,12 @@ namespace ConsoleApp1
                 }
             }
         }
-        class Bar4 : Foo4
+
+        public class Derived4 : Base4 // TODO OOP
         {
+            // override class Quux override для класса нельзя, а new можно (для метода и override, и new можно)
+            // class Quux предупреждение есть, protected class Quux предупреждения нет. связано с приватностью
+            //protected /*new*/ class Quux // protected new vs protected
             new class Quux
             {
                 public Quux()
@@ -124,48 +121,53 @@ namespace ConsoleApp1
                 }
             }
         }
-        class Baz4 : Bar4
+
+        public class Main4 : Derived4
         {
-            public Baz4()
+            public Main4()
             {
+                // Derived4.Quux private/internal(без мод-ра) -> Base4.Quux() - игнорируется в иерархии
+                // Derived4.Quux public/protected -> Base4.Quux() - ближайший доступный в иерархии
+                // Derived4.Quux protected new -> Base4.Quux() - ближайший доступный в иерархии, хоть и new
+                // можно не запускать - в дизайн тайме переходит куда надо по F12 по new Quux() здесь
                 new Quux();
             }
         }
 
-        class Foo5<T>
-        {
-            public static int Bar;
-        }
 
 
         public static string GetString(string s)
         {
-            Console.WriteLine("GetString: " + s);
+            //Console.WriteLine("GetString: " + s);
             return s;
         }
+
         public static IEnumerable<string> GetStringEnumerable()
         {
             yield return GetString("Foo");
             yield return GetString("Bar");
         }
+
         public static string[] EnumerableToArray()
         {
             var strings = GetStringEnumerable();
+
             foreach (var s in strings)
                 Console.WriteLine("EnumerableToArray: " + s);
+
             return strings.ToArray();
         }
 
-        static IEnumerable<string> Foo6()
+        public static IEnumerable<string> Foo6() // класс называть так нельзя
         {
             yield return "Bar";
             Console.WriteLine("Baz");
         }
 
-        static int Inc(int x)
+        public static int Inc(int x)
         {
-            Console.WriteLine("Inc: " + x);
-            return x /*+ 1*/;
+            //Console.WriteLine("Inc: " + x);
+            return x + 1;
         }
 
         public static IEnumerable<int> GetSmallNumbers()
@@ -188,9 +190,9 @@ namespace ConsoleApp1
             }
         }
 
-        static Action<int, int> print = (a, b) => Console.WriteLine("{0,2} = {1,2} * {2,3} + {3,3} ", a, b, a / b, a % b);
+        public static Action<int, int> print = (a, b) => Console.WriteLine("{0,2} = {1,2} * {2,3} + {3,3} ", a, b, a / b, a % b);
 
-        struct Foo7
+        public struct Foo7
         {
             int value;
             public override string ToString()
@@ -229,241 +231,6 @@ namespace ConsoleApp1
         }
 
         [ThreadStatic]
-        static readonly int Foo10 = 42;
-
-        static void Main(string[] args)
-        {
-            var list_ = new ConcurrentBag<string>();
-            string[] dirNames = { ".", ".." };
-            List<Task> tasks = new List<Task>();
-            foreach (var dirName in dirNames)
-            {
-                Task t = new Task(() => {
-                    foreach (var path in Directory.GetFiles(dirName))
-                        list_.Add(path);
-                });
-                tasks.Add(t);
-                t.Start();
-            }
-            Task.WaitAll(tasks.ToArray());
-            foreach (Task t in tasks)
-                Console.WriteLine("Task {0} Status: {1}", t.Id, t.Status);
-
-            Console.WriteLine("Number of files read: {0}", list_.Count);
-            Console.WriteLine("****************************************");
-
-
-            Console.WriteLine("|||||| ООП ||||||");
-            Console.WriteLine("--- 1. Что выведет следующий код? ---");
-            Foo();
-            Foo(null);
-            Foo(new Bar());
-            Foo(new Bar(), new Bar());
-            Foo(new Bar(), new object());
-
-            Console.WriteLine("--- 2. Что выведет следующий код? ---");
-            new Bar2().Quux(42);
-            new Baz().Quux(42);
-
-            //Bar2 a = new Bar2();
-            //Baz b = new Baz();
-            //a.Quux(42);
-            //b.Quux(42);
-            Console.WriteLine("--- 3. Что выведет следующий код? ---");
-            new Baz3();
-
-            Console.WriteLine("--- 4. Что выведет следующий код? ---");
-            new Baz4();
-
-            Console.WriteLine("--- 5. Что выведет следующий код? ---");
-            Foo5<int>.Bar++;
-            Console.WriteLine(Foo5<double>.Bar);
-
-            Console.WriteLine("|||||| LINQ ||||||");
-            Console.WriteLine("--- 1. Что выведет следующий код? ---");
-            EnumerableToArray();
-
-            Console.WriteLine("--- 2. Что выведет следующий код? ---");
-            foreach (var str in Foo6())
-                Console.Write(str);
-
-            Console.WriteLine("--- 5. Что выведет следующий код? ---");
-            var list = new List<string> { "Foo", "Bar", "Baz" };
-            var query = list.Where(c => c.StartsWith("B"));
-            list.Remove("Bar");
-            Console.WriteLine(query.Count());
-
-            Console.WriteLine("--- 6. Что выведет следующий код? ---");
-            var numbers = Enumerable.Range(0, 10);
-            var query2 =
-                (from number in numbers
-                 let number2 = Inc(number)
-                 where number2 % 2 == 0
-                 select number2)/*.Take(2)*/;
-            //var query2 =
-            //    (from number in numbers
-            //     where number % 2 == 0
-            //     select number);
-            foreach (var number in query2)
-                Console.WriteLine("Number: " + number);
-
-
-            Console.WriteLine("--- 6. В какой момент произойдёт Exception? ---");
-            var numbers2 = GetSmallNumbers();
-            var evenNumbers = numbers2.Select(n => n * 2);
-            Console.WriteLine(evenNumbers.FirstOrDefault());
-
-            Console.WriteLine("--- 7. Что выведет следующий код? ---");
-            Console.WriteLine(GetSmallNumbers2().First());
-
-            Console.WriteLine("|||||| Математика ||||||");
-            Console.WriteLine("1. Что выведет следующий код?");
-            Console.WriteLine(
-            "| Number | Round | Floor | Ceiling | Truncate | Format |");
-            foreach (var x in new[] { -2.9, -0.5, 0.3, 1.5, 2.5, 2.9 })
-            {
-                Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "| {0,6} | {1,5} | {2,5} | {3,7} | {4,8} | {0,6:N0} |", x, Math.Round(x), Math.Floor(x), Math.Ceiling(x), Math.Truncate(x)));
-            }
-
-            Console.WriteLine("2. Что выведет следующий код?");
-            Console.WriteLine(" a = b * (a/b) + (a%b)");
-            print(7, 3);
-            print(7, -3);
-            print(-7, 3);
-            print(-7, -3);
-
-            Console.WriteLine("3. Что выведет следующий код?");
-            Console.WriteLine("0.1 + 0.2 {0} 0.3", 0.1 + 0.2 == 0.3 ? "==" : "!=");
-
-            Console.WriteLine("4. Что выведет следующий код?");
-            var zero = 0;
-            try
-            {
-                Console.WriteLine(42 / 0.0);
-                Console.WriteLine(42.0 / 0);
-                Console.WriteLine(42 / zero);
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("DivideByZeroException");
-            }
-
-            Console.WriteLine("5. Что выведет следующий код?");
-            int ten = 10;
-            Console.WriteLine(unchecked(2147483647 + ten));
-            var maxInt32 = Int32.MaxValue;
-            var maxDouble = Double.MaxValue;
-            var maxDecimal = Decimal.MaxValue;
-            var maxByte = Byte.MaxValue;
-            checked
-            {
-                Console.Write("Checked Int32 increased max: ");
-                try { Console.WriteLine(maxInt32 + 42); }
-                catch { Console.WriteLine("OverflowException"); }
-                Console.Write("Checked Double increased max: ");
-                try { Console.WriteLine(maxDouble + 42); }
-                catch { Console.WriteLine("OverflowException"); }
-                Console.Write("Checked Decimal increased max: ");
-                try { Console.WriteLine(maxDecimal + 42); }
-                catch { Console.WriteLine("OverflowException"); }
-            }
-            unchecked
-            {
-                Console.Write("Unchecked Int32 increased max: ");
-                try { Console.WriteLine(maxInt32 + 42); }
-                catch { Console.WriteLine("OverflowException"); }
-                Console.Write("Unchecked Double increased max: ");
-                try { Console.WriteLine(maxDouble + 42); }
-                catch { Console.WriteLine("OverflowException"); }
-                Console.Write("Unchecked Decimal increased max: ");
-                try { Console.WriteLine(maxDecimal + 42); }
-                catch { Console.WriteLine("OverflowException"); }
-            }
-
-            Console.WriteLine("7. Что выведет следующий код?");
-            byte foo = 1;
-            dynamic bar = foo;
-            Console.WriteLine(bar.GetType());
-            bar += foo;
-            Console.WriteLine(bar.GetType());
-
-
-            Console.WriteLine("|||||| Значимые типы ||||||");
-            Console.WriteLine("1. Что выведет следующий код?");
-            var foo7 = new Foo7();
-            Console.WriteLine(foo7);
-            Console.WriteLine(foo7);
-            object bar2 = foo7;
-            object qux = foo7;
-            object baz = bar;
-            Console.WriteLine(baz);
-            Console.WriteLine(bar2);
-            Console.WriteLine(baz);
-            Console.WriteLine(qux);
-
-            Console.WriteLine("2. Что выведет следующий код?");
-            var bar5 = new Bar5 { Foo8 = new Foo8() };
-            bar5.Foo8.Change(5);
-            Console.WriteLine(bar5.Foo8.Value);
-
-            Console.WriteLine("3. Что выведет следующий код?");
-            var x2 = new
-            {
-                Items = new List<int> { 1, 2, 3 }.GetEnumerator()
-            };
-
-            //while (x2.Items.MoveNext())
-            //    Console.WriteLine(x2.Items.Current);
-
-            Console.WriteLine("4. Что выведет следующий код?");
-            Console.WriteLine(Marshal.SizeOf(typeof(Foo9)));
-            //Console.WriteLine(Marshal.SizeOf(typeof(Bar6)));
-            //Console.WriteLine(Marshal.SizeOf(typeof(Int32)));
-
-
-            Console.WriteLine("|||||| Строки ||||||");
-            Console.WriteLine("1. Что выведет следующий код?");
-            Console.WriteLine(1 + 2 + "A");
-            Console.WriteLine(1 + "A" + 2);
-            Console.WriteLine("A" + 1 + 2);
-
-            Console.WriteLine("2. Что выведет следующий код?");
-            Console.WriteLine(1 + 2 + 'A');
-            Console.WriteLine(1 + 'A' + 2);
-            Console.WriteLine('A' + 1 + 2);
-
-            Console.WriteLine("3. Что выведет следующий код?");
-
-            //string aaa = "hello world";
-            //string bbb = aaa;
-            //aaa = aaa.Replace("h", "a");
-            //var aaa2 = aaa;
-            //var bbb2 = bbb;
-
-            //string aaa = "hello world";
-            //string bbb = "hello world";
-            //aaa = null;
-            //var aaa2 = aaa == bbb;
-            //var bbb2 = (object)aaa == (object)bbb;
-
-            Console.WriteLine("9. Что выведет следующий код?");
-            var x3 = "AB";
-            var y = new StringBuilder().Append('A').Append('B').ToString();
-            var z = string.Intern(y);
-            Console.WriteLine(x3 == y);
-            Console.WriteLine(x3 == z);
-            Console.WriteLine((object)x3 == (object)y);
-            Console.WriteLine((object)x3 == (object)z);
-
-
-            Console.WriteLine("|||||| Многопоточность ||||||");
-            Console.WriteLine("1. Что выведет следущий код?");
-            var thread = new Thread(() => Console.WriteLine(Foo10));
-            thread.Start();
-            thread.Join();
-            Console.ReadLine();
-        }
-
-
+        public static readonly int Foo10 = 42;
     }
 }
---
+++ problembook.net/ProgramMain.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections.Concurrent;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+using static ConsoleApp1.Program; // TODO это надо для partial?
+
+namespace problembook.net
+{
+    partial class Program
+    {
+
+        public class Foo5<T>
+        {
+            public static int Bar; // TODO в другом partial классе в дебаге не видно значение
+            //public static T Bar;
+        }
+
+        static void Main(string[] args)
+        {
+            var list_ = new ConcurrentBag<string>();
+            string[] dirNames = { ".", ".." };
+            List<Task> tasks = new List<Task>();
+            foreach (var dirName in dirNames)
+            {
+                Task t = new Task(() =>
+                {
+                    foreach (var path in Directory.GetFiles(dirName))
+                        list_.Add(path);
+                });
+                tasks.Add(t);
+                t.Start();
+            }
+            Task.WaitAll(tasks.ToArray());
+            foreach (Task t in tasks)
+                Console.WriteLine("Task {0} Status: {1}", t.Id, t.Status);
+
+            Console.WriteLine("Number of files read: {0}", list_.Count);
+            Console.WriteLine("****************************************");
+
+
+            Console.WriteLine("*** ООП ***");
+            Console.WriteLine("*** 1. Что выведет следующий код? ***");
+            Foo();
+            Foo(null);
+            Foo(new Bar());
+            Foo(new Bar(), new Bar());
+            Foo(new Bar(), new object());
+
+            Console.WriteLine("*** 2. Что выведет следующий код? ***");
+            new Bar2().Quux(42);
+            new Baz().Quux(42);
+
+            //Bar2 a = new Bar2();
+            //Baz b = new Baz();
+            //a.Quux(42);
+            //b.Quux(42);
+            Console.WriteLine("*** 3. Что выведет следующий код? ***");
+            new Baz3();
+
+            Console.WriteLine("*** 4. Что выведет следующий код? ***");
+            new Main4();
+
+            Console.WriteLine("*** 5. Что выведет следующий код? ***");
+            Foo5<int>.Bar++;
+            Console.WriteLine(Foo5<double>.Bar); // 0 ? TODO
+            Console.WriteLine(Foo5<int>.Bar); // 1 ?
+
+            Console.WriteLine("|||||| LINQ ||||||");
+            Console.WriteLine("*** 1. Что выведет следующий код? ***");
+            EnumerableToArray(); // Foo, Bar
+
+            Console.WriteLine("*** 2. Что выведет следующий код? ***");
+            foreach (var str in Foo6()) Console.Write(str); // BarBaz TODO ?
+
+            Console.WriteLine("*** 5. Что выведет следующий код? ***");
+            var list = new List<string> { "Foo", "Bar", "Baz" };
+            var query = list.Where(c => c.StartsWith("B"));
+            list.Remove("Bar");
+            Console.WriteLine(query.Count()); // слишком очевидно
+
+            Console.WriteLine("*** 6. Что выведет следующий код? ***"); // TODO п2
+            var numbers = Enumerable.Range(0, 10);
+            // 0 1 2 3 4 5 6 7 8 9 
+            var query2 =
+                (from number in numbers
+                 let number2 = Inc(number)
+                 where number2 % 2 == 0
+                 select number2)/*.Take(2)*/;
+            // 1 2 3 4 5 6 7 8 9 10
+            //var query2 = from number in numbers where number % 2 == 0 select number;
+            foreach (var number in query2) Console.WriteLine("Number: " + number);
+            // 2 4 6 8 10
+
+            Console.WriteLine("*** 6. В какой момент произойдёт Exception? ***");
+            var numbers2 = GetSmallNumbers(); // искл нет
+            var evenNumbers = numbers2.Select(n => n * 2); // искл нет
+            Console.WriteLine(evenNumbers.First()); // 2 искл нет First() - метод расширения IEnumerable, lazy, искл не фиксируется
+            Console.WriteLine(evenNumbers.FirstOrDefault()); // 2 искл нет
+            //evenNumbers.ToArray(); // искл
+
+            Console.WriteLine("*** 7. Что выведет следующий код? ***");
+            // штатно вернётся коллекция (итеративная часть метода), штатно отработает finally (оставшаяся часть метода)
+            Console.WriteLine(GetSmallNumbers2().First()); // Foo 1 - почему такой порядок? TODO
+
+            Console.WriteLine("|||||| Математика ||||||"); // TODO п2
+            Console.WriteLine("1. Что выведет следующий код?");
+            Console.WriteLine("| Number | Round | Floor | Ceiling | Truncate | Format |");
+            foreach (var x in new[] { -2.9, -0.5, 0.3, 1.5, 2.5, 2.9 })
+                Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "| {0,6} | {1,5} | {2,5} | {3,7} | {4,8} | {0,6:N0} |", x, Math.Round(x), Math.Floor(x), Math.Ceiling(x), Math.Truncate(x)));
+
+            Console.WriteLine("2. Что выведет следующий код?");
+            Console.WriteLine(" a = b * (a/b) + (a%b)");
+            print(7, 3);
+            print(7, -3);
+            print(-7, 3);
+            print(-7, -3);
+
+            Console.WriteLine("3. Что выведет следующий код?");
+            Console.WriteLine("0.1 + 0.2 {0} 0.3", 0.1 + 0.2 == 0.3 ? "==" : "!=");
+
+            Console.WriteLine("4. Что выведет следующий код?");
+            var zero = 0;
+            try
+            {
+                Console.WriteLine(42 / 0.0);
+                Console.WriteLine(42.0 / 0);
+                Console.WriteLine(42 / zero);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("DivideByZeroException");
+            }
+
+            Console.WriteLine("5. Что выведет следующий код?");
+            int ten = 10;
+            Console.WriteLine(unchecked(2147483647 + ten));
+            var maxInt32 = Int32.MaxValue;
+            var maxDouble = Double.MaxValue;
+            var maxDecimal = Decimal.MaxValue;
+            var maxByte = Byte.MaxValue;
+            checked
+            {
+                Console.Write("Checked Int32 increased max: ");
+                try { Console.WriteLine(maxInt32 + 42); }
+                catch { Console.WriteLine("OverflowException"); }
+                Console.Write("Checked Double increased max: ");
+                try { Console.WriteLine(maxDouble + 42); }
+                catch { Console.WriteLine("OverflowException"); }
+                Console.Write("Checked Decimal increased max: ");
+                try { Console.WriteLine(maxDecimal + 42); }
+                catch { Console.WriteLine("OverflowException"); }
+            }
+            unchecked
+            {
+                Console.Write("Unchecked Int32 increased max: ");
+                try { Console.WriteLine(maxInt32 + 42); }
+                catch { Console.WriteLine("OverflowException"); }
+                Console.Write("Unchecked Double increased max: ");
+                try { Console.WriteLine(maxDouble + 42); }
+                catch { Console.WriteLine("OverflowException"); }
+                Console.Write("Unchecked Decimal increased max: ");
+                try { Console.WriteLine(maxDecimal + 42); }
+                catch { Console.WriteLine("OverflowException"); }
+            }
+
+            Console.WriteLine("7. Что выведет следующий код?");
+            byte foo = 1;
+            dynamic bar = foo;
+            //object bar = foo;
+            Console.WriteLine(bar.GetType()); // System.Byte
+            bar += foo;
+            Console.WriteLine(bar.GetType()); // System.Int32 из-за +?
+
+
+            Console.WriteLine("|||||| Значимые типы ||||||");
+            Console.WriteLine("1. Что выведет следующий код?");
+            var foo7 = new Foo7();
+            Console.WriteLine(foo7);
+            object bar2 = foo7;
+            object qux = foo7;
+            object baz = bar;
+            Console.WriteLine(baz);
+            Console.WriteLine(bar2);
+            Console.WriteLine(baz);
+            Console.WriteLine(qux);
+
+            Console.WriteLine("2. Что выведет следующий код?");
+            var bar5 = new Bar5 { Foo8 = new Foo8() };
+            bar5.Foo8.Change(5);
+            Console.WriteLine(bar5.Foo8.Value);
+
+            Console.WriteLine("3. Что выведет следующий код?");
+            var x2 = new
+            {
+                Items = new List<int> { 1, 2, 3 }.GetEnumerator()
+            };
+
+            //while (x2.Items.MoveNext())
+            //    Console.WriteLine(x2.Items.Current);
+
+            Console.WriteLine("4. Что выведет следующий код?");
+            Console.WriteLine(Marshal.SizeOf(typeof(Foo9)));
+            //Console.WriteLine(Marshal.SizeOf(typeof(Bar6)));
+            //Console.WriteLine(Marshal.SizeOf(typeof(Int32)));
+
+
+            Console.WriteLine("|||||| Строки ||||||");
+            Console.WriteLine("1. Что выведет следующий код?");
+            Console.WriteLine(1 + 2 + "A");
+            Console.WriteLine(1 + "A" + 2);
+            Console.WriteLine("A" + 1 + 2);
+
+            Console.WriteLine("2. Что выведет следующий код?");
+            Console.WriteLine(1 + 2 + 'A');
+            Console.WriteLine(1 + 'A' + 2);
+            Console.WriteLine('A' + 1 + 2);
+
+            Console.WriteLine("3. Что выведет следующий код?");
+
+            //string aaa = "hello world";
+            //string bbb = aaa;
+            //aaa = aaa.Replace("h", "a");
+            //var aaa2 = aaa;
+            //var bbb2 = bbb;
+
+            //string aaa = "hello world";
+            //string bbb = "hello world";
+            //aaa = null;
+            //var aaa2 = aaa == bbb;
+            //var bbb2 = (object)aaa == (object)bbb;
+
+            Console.WriteLine("9. Что выведет следующий код?");
+            var x3 = "AB";
+            var y = new StringBuilder().Append('A').Append('B').ToString();
+            var z = string.Intern(y);
+            Console.WriteLine(x3 == y);
+            Console.WriteLine(x3 == z);
+            Console.WriteLine((object)x3 == (object)y);
+            Console.WriteLine((object)x3 == (object)z);
+
+
+            Console.WriteLine("|||||| Многопоточность ||||||");
+            Console.WriteLine("1. Что выведет следущий код?");
+            var thread = new Thread(() => Console.WriteLine(Foo10));
+            thread.Start();
+            thread.Join();
+            Console.ReadLine();
+        }
+    }
+}
