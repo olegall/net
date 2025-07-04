5f48a2fbd47d50c50316c1e4ee344b5ebeb556df
oleg aleynikov <aleynikov@raduga-lik.ru>
2025-07-04 16:18:14 +0300

    ref

---
+++ ref/Program.cs
@@ -0,0 +1,20 @@
+﻿void SetName(ref A a) // ссылка на ссылку. без ref предупреждение на последней строке и она не повлияет
+{
+    a.Id = 0;
+    a.Name = null;
+
+    // созд-ся новый объект, a иниц-ся его адресом.
+    // без ref: отрыв от 1-го объекта, изменения на него не повлияют.
+    // c ref: подкапотная ссылка, указывает сначала на изначальный a, потом на изменённый a TODO
+    a = new A{ Id = 2, Name = "Pete"}; 
+}
+
+var a = new A{ Id = 1, Name = "Oleg"};
+SetName(ref a);
+int break_ = 0;
+
+class A
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
